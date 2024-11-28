be19b6d59767dda76f87ec29bef8dd3347bc41c0
Abdulla Mohamed <46408571+IbeAbdullaGit@users.noreply.github.com>
2024-11-28 14:09:40 -0500

    TestingNavbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <header className='flex justify-between items-center sm:px-16 px-8 py-4 mx-auto top-0 bg-transparent z-10 right-0 left-0 '>
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md" >
+        <NavLink to="/Portfolio" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md" >
         <p className='blue-gradient-text'>AM</p>
         </NavLink>
         <nav className='flex text-lg gap-7 font-medium'>
