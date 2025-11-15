f5bf70ccfb01794469427406ddea58c080b22269
Abdulla Mohamed <46408571+IbeAbdullaGit@users.noreply.github.com>
2025-11-15 17:24:47 -0500

    Removed About, Contact, and Projects tabs from the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Home, About, Contact, Projects, ProjectNexusESC, ProjectSpinCritters,ProjectRubbishRush, ProjectTheBeyond, ProjectXelify, ProjectBoardGameDesigner, ProjectWavesTD} from "./pages";
+import { Home, ProjectNexusESC, ProjectSpinCritters,ProjectRubbishRush, ProjectTheBeyond, ProjectXelify, ProjectBoardGameDesigner, ProjectWavesTD} from "./pages";
 import { Navbar } from "./components/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
 
@@ -13,9 +13,6 @@ const App = () => {
         <ScrollToTop />
         <Routes>
           <Route path="/Portfolio/" element={<Home/>}/>
-          <Route path="/Portfolio/about" element={<About/>}/>
-          <Route path="/Portfolio/projects" element={<Projects/>}/>
-          <Route path="/Portfolio/contact" element={<Contact/>}/>
           <Route path="/Portfolio/project/nexus-esc" element={<ProjectNexusESC/>}/>
           <Route path="/Portfolio/project/spin-critters" element={<ProjectSpinCritters/>}/>
           <Route path="/Portfolio/project/rubbish-rush" element={<ProjectRubbishRush/>}/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Projects } from './Projects';
-export { default as Contact } from './Contact';
 export { default as ProjectNexusESC } from './ProjectNexusESC';
 export { default as ProjectSpinCritters } from './ProjectSpinCritters';
 export { default as ProjectTheBeyond } from './ProjectTheBeyond';
